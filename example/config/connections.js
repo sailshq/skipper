f617d94804756b7f87a4b38d47244d2d22c10584
Mike McNeil <michael.r.mcneil@gmail.com>
2014-02-11 15:53:40 -0600

    Update connections config in example to use the default upload location.

--- example/config/connections.js
+++ example/config/connections.js
@@ -17,7 +17,7 @@ module.exports.connections = {
   // Local disk storage for file uploads.
   tmpDirectoryOnLocalFilesystem: {
     adapter: 'sails-local-fs',
-    pathPrefix: '.tmp/foo'
+    pathPrefix: '.tmp/'
   },
 
 
