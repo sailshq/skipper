21427b2138a1073f02bf79554234d51b72844153
Mike McNeil <michael.r.mcneil@gmail.com>
2014-02-16 00:40:04 -0600

    Much more complete error handling.

--- test/helpers/receiver.js
+++ test/helpers/receiver.js
@@ -32,13 +32,29 @@ module.exports = {
 
 			console.log('Receiver: Received file `'+__newFile.filename+'` from an Upstream.');
 
+			// Listen for errors on the incoming side of this file stream
+			// (i.e. if the user cancelled the upload)
+			__newFile.on('error', function (err) {
+				console.error('Receiver: Error on incoming stream received for `'+__newFile.filename+'`::', err, ' :: Cancelling upload and cleaning up already-written bytes...');
+				//
+				// TODO:
+				// In a real receiver, this is where the already-written bytes
+				// for this file would be garbage collected.
+				// 
+			});
+
 			var outs = __newFile.pipe(require('fs').createWriteStream('/dev/null'));
 			outs.on('finish', function () {
 				console.log('Receiver: Finished writing `'+__newFile.filename+'`');
 				next();
 			});
 			outs.on('error', function (err) {
-				console.error('Receiver: Error writing `'+__newFile.filename+'`::', err);
+				console.error('Receiver: Error writing `'+__newFile.filename+'`::', err, ' :: Cancelling upload and cleaning up already-written bytes...');
+				//
+				// TODO:
+				// In a real receiver, this is where the already-written bytes
+				// for this file would be garbage collected.
+				// 
 				next(err);
 			});
 		};
