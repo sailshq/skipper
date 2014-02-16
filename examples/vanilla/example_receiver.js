3c47e48268a90205aa504b18bf8de5ddfc9aef4b
Mike McNeil <michael.r.mcneil@gmail.com>
2014-02-16 11:00:49 -0600

    Set up vanilla example.

--- examples/vanilla/example_receiver.js
+++ examples/vanilla/example_receiver.js
@@ -3,6 +3,7 @@
  */
 
 var Writable = require('stream').Writable;
+require('colors');
 
 
 
@@ -20,17 +21,17 @@ module.exports = function newReceiverStream (options) {
 
 	receiver__._write = function onFile (__newFile, encoding, next) {
 
-		log(('Receiver: Received file `'+__newFile.filename+'` from an Upstream.').grey);
+		console.log(('Receiver: Received file `'+__newFile.filename+'` from an Upstream.').grey);
 
 		// Default the output path for files to `/dev/null` for testing purposes.
 		var outputPath = options.outputPath || '/dev/null';
 		var outs = __newFile.pipe(require('fs').createWriteStream(outputPath));
 		outs.on('finish', function () {
-			log(('Receiver: Finished writing `'+__newFile.filename+'`').grey);
+			console.log(('Receiver: Finished writing `'+__newFile.filename+'`').grey);
 			next();
 		});
 		outs.on('error', function (err) {
-			log(('Receiver: Error writing `'+__newFile.filename+'`:: '+ require('util').inspect(err)+' :: Cancelling upload and cleaning up already-written bytes...').red);
+			console.log(('Receiver: Error writing `'+__newFile.filename+'`:: '+ require('util').inspect(err)+' :: Cancelling upload and cleaning up already-written bytes...').red);
 			//
 			// TODO:
 			// In a real receiver, this is where the already-written bytes
