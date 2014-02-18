0c81ba8d1e36134cbbb870e3a888b8dd97856957
Mike McNeil <michael.r.mcneil@gmail.com>
2014-02-17 21:24:56 -0600

    Added more tests, some of them with a random element.

--- test/req.file.ignore-test.js
+++ test/req.file.ignore-test.js
@@ -46,12 +46,8 @@ describe('Ignoring :: req.file("foo"), when a file upload is sent to the "bar" f
 
 	});
 
-	it('should NOT have uploaded the file to `suite.outputDir`', function () {
+	IT_SHOULD_NOT_HAVE_UPLOADED_ANY_FILES_TO_OUTPUTDIR();
 
-		// Check that nothing was uploaded
-		var filesUploaded = fsx.readdirSync(suite.outputDir.path);
-		assert(filesUploaded.length === 0);
-	});
 
 
 
@@ -59,28 +55,94 @@ describe('Ignoring :: req.file("foo"), when a file upload is sent to the "bar" f
 		suite.app.post('/noop', actionFixtures.send200);
 	});
 
-	it('sends an attachment on an unused field', function(done) {		
+	IT_SENDS_MANY_ATTACHMENTS({ url: 'http://localhost:3000/noop' });
+
+	IT_SHOULD_NOT_HAVE_UPLOADED_ANY_FILES_TO_OUTPUTDIR();
+
+
+
+
+
+	it('binds an action which does not do anything w/ ANY of the incoming Upstreams (+ introduces a random delay <=2 seconds)', function () {
 		
-		// Builds an HTTP request
-		var httpRequest = Uploader({
-			url: 'http://localhost:3000/noop'
-		}, toValidateTheHTTPResponse(done));
+		suite.app.post('/noop2', function (req, res, next) {
+			req.__FILE_PARSER_TESTS__DELAY = Math.floor(Math.random() * 1700);
+			next();
+		});
 
-		// Attaches a few files to the HTTP request.,
-		var form = httpRequest.form();
-		var pathToSmallFile = suite.srcFiles[1].path;
-		form.append('something_we_dont_care_about', fsx.createReadStream(pathToSmallFile));
-		form.append('something_we_dont_care_about', fsx.createReadStream(pathToSmallFile));
-		form.append('foo', fsx.createReadStream(pathToSmallFile));
-		form.append('bar', fsx.createReadStream(pathToSmallFile));
-
+		suite.app.post('/noop2', actionFixtures.send200);
 	});
 
-	it('should NOT have uploaded the file to `suite.outputDir`', function () {
 
-		// Check that nothing was uploaded
-		var filesUploaded = fsx.readdirSync(suite.outputDir.path);
-		assert(filesUploaded.length === 0);
-	});
+	IT_SENDS_MANY_ATTACHMENTS({ url: 'http://localhost:3000/noop2' });
+	IT_SENDS_MANY_ATTACHMENTS({ url: 'http://localhost:3000/noop2' });
+	IT_SENDS_MANY_ATTACHMENTS({ url: 'http://localhost:3000/noop2' });
+	IT_SENDS_MANY_ATTACHMENTS({ url: 'http://localhost:3000/noop2' });
+	IT_SENDS_MANY_ATTACHMENTS({ url: 'http://localhost:3000/noop2' });
+	IT_SENDS_MANY_ATTACHMENTS({ url: 'http://localhost:3000/noop2' });
+	IT_SENDS_MANY_ATTACHMENTS({ url: 'http://localhost:3000/noop2' });
+	IT_SENDS_MANY_ATTACHMENTS({ url: 'http://localhost:3000/noop2' });
+	IT_SENDS_MANY_ATTACHMENTS({ url: 'http://localhost:3000/noop2' });
+	IT_SENDS_MANY_ATTACHMENTS({ url: 'http://localhost:3000/noop2' });
+	IT_SENDS_MANY_ATTACHMENTS({ url: 'http://localhost:3000/noop2' });
+	IT_SENDS_MANY_ATTACHMENTS({ url: 'http://localhost:3000/noop2' });
+	IT_SENDS_MANY_ATTACHMENTS({ url: 'http://localhost:3000/noop2' });
+	IT_SENDS_MANY_ATTACHMENTS({ url: 'http://localhost:3000/noop2' });
+	IT_SENDS_MANY_ATTACHMENTS({ url: 'http://localhost:3000/noop2' });
+	IT_SENDS_MANY_ATTACHMENTS({ url: 'http://localhost:3000/noop2' });
+	IT_SENDS_MANY_ATTACHMENTS({ url: 'http://localhost:3000/noop2' });
+	IT_SENDS_MANY_ATTACHMENTS({ url: 'http://localhost:3000/noop2' });
+	IT_SENDS_MANY_ATTACHMENTS({ url: 'http://localhost:3000/noop2' });
+	IT_SENDS_MANY_ATTACHMENTS({ url: 'http://localhost:3000/noop2' });
+	IT_SENDS_MANY_ATTACHMENTS({ url: 'http://localhost:3000/noop2' });
+	IT_SENDS_MANY_ATTACHMENTS({ url: 'http://localhost:3000/noop2' });
+
+	IT_SHOULD_NOT_HAVE_UPLOADED_ANY_FILES_TO_OUTPUTDIR();
+
+
+
+	
+	/**
+	 * [IT_SENDS_MANY_ATTACHMENTS description]
+	 * @param {[type]} options [description]
+	 */
+	function IT_SENDS_MANY_ATTACHMENTS (options) {
+
+		it('sends many attachments on unused fields', function (done) {		
+			
+			// Builds an HTTP request
+			var httpRequest = Uploader({
+				url: options.url
+			}, toValidateTheHTTPResponse(done));
+
+			// Attaches a few files to the HTTP request.,
+			var form = httpRequest.form();
+			var pathToSmallFile = suite.srcFiles[1].path;
+			form.append('something_we_dont_care_about', fsx.createReadStream(pathToSmallFile));
+			form.append('something_we_dont_care_about', fsx.createReadStream(pathToSmallFile));
+			form.append('foo', fsx.createReadStream(pathToSmallFile));
+			form.append('bar', fsx.createReadStream(pathToSmallFile));
+		});
+
+	}
+
+
+	/**
+	 * [IT_SHOULD_NOT_HAVE_UPLOADED_ANY_FILES_TO_OUTPUTDIR description]
+	 * @param {[type]} options [description]
+	 */
+	function IT_SHOULD_NOT_HAVE_UPLOADED_ANY_FILES_TO_OUTPUTDIR (options) {
+		it('should NOT have uploaded any files to `suite.outputDir`', function () {
+
+			// Check that nothing was uploaded
+			var filesUploaded = fsx.readdirSync(suite.outputDir.path);
+			assert(filesUploaded.length === 0);
+		});
+	}
 
 });
+
+
+
+
+
