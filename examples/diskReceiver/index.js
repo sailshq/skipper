ef05c486888c92e45ec92c50f7415f055fa465e2
Mike McNeil <mike@balderdash.co>
2014-04-13 01:43:14 -0500

    Update example disk receiver.

--- examples/diskReceiver/index.js
+++ examples/diskReceiver/index.js
@@ -3,9 +3,9 @@
  */
 
 var Writable = require('stream').Writable;
-var fs = require('fs');
-require('colors');
-
+var fsx = require('fs-extra');
+var path = require('path');
+var _ = require('lodash');
 
 
 
@@ -17,50 +17,72 @@ require('colors');
  * This is just a super-basic thing that writes files to disk.
  * It does include a garbage-collection mechanism for file uploads
  * which were not successful.
- * 
+ *
  * @return {Stream.Writable}
  */
 
-module.exports = function newReceiverStream (options) {
-	options = options || {};
+module.exports = function newReceiverStream(options) {
+  options = options || {};
+  _.defaults(options, {
 
-	// Default the output path for files to `/dev/null` if no `id` option
-	// is passed in (for testing purposes only)
-	var filePath = options.id || '/dev/null';
+    // By default, create new files on disk
+    // using their uploaded filenames.
+    // (no overwrite-checking is performed!!)
+    getFilename: function (__newFile) {
+      return __newFile.filename;
+    },
 
-	var receiver__ = Writable({objectMode: true});
+    // By default, upload files to `./.tmp/uploads` (relative to cwd)
+    dirname: '.tmp/uploads'
+  });
 
-	// This `_write` method is invoked each time a new file is received
-	// from the Readable stream (Upstream) which is pumping filestreams
-	// into this receiver.  (filename === `__newFile.filename`).
-	receiver__._write = function onFile (__newFile, encoding, done) {
-		var outs = fs.createWriteStream(filePath, encoding);
-		__newFile.pipe(outs);
+  var receiver__ = Writable({
+    objectMode: true
+  });
 
-		// Garbage-collect the bytes that were already written for this file.
-		// (called when a read or write error occurs)
-		function gc (err) {
-			console.log('************** Garbage collecting file `'+__newFile.filename+'` located @ '+filePath+'...');
-			fs.unlink(filePath, function (gcErr) {
-				if (gcErr) return done([err].concat([gcErr]));
-				return done(err);
-			});
-		}
+  // This `_write` method is invoked each time a new file is received
+  // from the Readable stream (Upstream) which is pumping filestreams
+  // into this receiver.  (filename === `__newFile.filename`).
+  receiver__._write = function onFile(__newFile, encoding, done) {
+
+    // Determine location where file should be written
+    var dirPath = path.resolve(options.dirname);
+    var filename = options.getFilename(__newFile);
+    var filePath = path.join(dirPath, filename);
+
+    // Garbage-collect the bytes that were already written for this file.
+    // (called when a read or write error occurs)
+    function gc(err) {
+      // console.log('************** Garbage collecting file `' + __newFile.filename + '` located @ ' + filePath + '...');
+      fsx.unlink(filePath, function(gcErr) {
+        if (gcErr) return done([err].concat([gcErr]));
+        return done(err);
+      });
+    }
+
+    // Ensure necessary parent directories exist:
+    fsx.mkdirs(dirPath, function (err) {
+      if (err) { /* ignore mkdir error */ }
+      
+      var outs = fsx.createWriteStream(filePath, encoding);
+      __newFile.pipe(outs);
 
 
-		__newFile.on('error', function (err) {
-			console.log('***** READ error on file '+__newFile.filename, '::',err);
-		});
-		outs.on('error', function failedToWriteFile (err) {
-			console.log('Error on output stream- garbage collecting unfinished uploads...');
-			gc(err);
-		});
 
-		outs.on('finish', function successfullyWroteFile () {
-			done();
-		});
+      __newFile.on('error', function(err) {
+        // console.log('***** READ error on file ' + __newFile.filename, '::', err);
+      });
+      outs.on('error', function failedToWriteFile(err) {
+        // console.log('Error on output stream- garbage collecting unfinished uploads...');
+        gc(err);
+      });
 
-	};
-	
-	return receiver__;
+      outs.on('finish', function successfullyWroteFile() {
+        done();
+      });
+    });
+
+  };
+
+  return receiver__;
 };
