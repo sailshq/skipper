a37226fec1f7e2e8fb5a78c85f2903f5c37fd8f0
Mike McNeil <mike@balderdash.co>
2014-07-30 01:43:07 -0500

    All tests pass again w/ new skipper disk work

--- test/default.adapter.test.js
+++ test/default.adapter.test.js
@@ -2,14 +2,14 @@
  * Module dependencies
  */
 
-var Lifecycle = require('./helpers/lifecycle')
-  , Uploader = require('./helpers/uploader')
-  , _ = require('lodash')
-  , util = require('util')
-  , path = require('path')
-  , assert = require('assert')
-  , toValidateTheHTTPResponse = require('./helpers/toValidateTheHTTPResponse')
-  , fsx = require('fs-extra');
+var Lifecycle = require('./helpers/lifecycle');
+var Uploader = require('./helpers/uploader');
+var _ = require('lodash');
+var util = require('util');
+var path = require('path');
+var assert = require('assert');
+var toValidateTheHTTPResponse = require('./helpers/toValidateTheHTTPResponse');
+var fsx = require('fs-extra');
 
 
 // Fixtures
@@ -23,14 +23,14 @@ describe('req.file(...).upload() defaults to skipper-disk adapter when passed a 
   before(suite.setup);
   after(suite.teardown);
 
-  it('binds a file uploader action', function () {
-    suite.app.post('/upload', function (req, res) {
+  it('binds a file uploader action', function() {
+    suite.app.post('/upload', function(req, res) {
       bodyParamsThatWereAccessible = _.cloneDeep(req.body);
 
       var OUTPUT_PATH = req.__FILE_PARSER_TESTS__OUTPUT_PATH__AVATAR;
 
       req.file('avatar')
-        .upload(OUTPUT_PATH, function (err, files) {
+        .upload(OUTPUT_PATH, function(err, files) {
           if (err) {
             try {
               if (err instanceof Error) {
@@ -66,16 +66,16 @@ describe('req.file(...).upload() defaults to skipper-disk adapter when passed a 
   });
 
 
-  it('should have uploaded a file to `suite.outputDir`', function () {
+  it('should have uploaded a file to `suite.outputDir`', function() {
 
     // Check that a file landed
     var filesUploaded = fsx.readdirSync(suite.outputDir.path);
-    assert(filesUploaded.length === 1);
+    assert.equal(filesUploaded.length,1, 'Expected `filesUploaded.length` === 1, but it was '+filesUploaded.length+'\nHere is the complete list: \n'+util.inspect(filesUploaded, false, null));
 
     // Check that its contents are correct
     var uploadedFileContents = fsx.readFileSync(path.join(suite.outputDir.path, filesUploaded[0]));
     var srcFileContents = fsx.readFileSync(suite.srcFiles[0].path);
-    assert( uploadedFileContents.toString() === srcFileContents.toString() );
+    assert(uploadedFileContents.toString() === srcFileContents.toString());
   });
 
 
--- test/sucessive.file.upload.test.js
+++ test/sucessive.file.upload.test.js
@@ -29,13 +29,11 @@ describe('Sucessive file uploads should not fail ::', function() {
     suite.app.post('/upload', function (req, res) {
       bodyParamsThatWereAccessible = _.cloneDeep(req.body);
 
-      // var OUTPUT_PATH = req.__FILE_PARSER_TESTS__OUTPUT_PATH + "/" + guid.raw() + ".jpg";
-
       req.file('avatar')
         .upload({
           maxBytes: 150000000, // 150MB
           dirname: req.__FILE_PARSER_TESTS__OUTPUT_PATH,
-          filename: guid.raw()+'.jpg'
+          saveAs: guid.raw()+'.jpg'
         }, function (err, files) {
           if (err) res.send(500, err);
           res.send(200);
@@ -126,7 +124,7 @@ describe('Sucessive file uploads should not fail ::', function() {
           form.append('avatar', fsx.createReadStream(pathToSmallFile));
         }
       ], function(err, result){
-        done();
+        done(err);
       });
 
   });
@@ -136,7 +134,8 @@ describe('Sucessive file uploads should not fail ::', function() {
 
     // Check that a file landed
     var filesUploaded = fsx.readdirSync(suite.outputDir.path);
-    assert(filesUploaded.length === 6);
+    console.log('CHECKING IN ',suite.outputDir.path);
+    assert.equal(filesUploaded.length, 6);
 
     for(var i = 0; i < 6; i++){
       // Check that its contents are correct
