3d81c6f3f7bc8547c46d164092348bbdd3c83987
Mike McNeil <mike@balderdash.co>
2015-01-23 09:55:57 -0800

    Fix syntax in previous commit

--- standalone/logger.js
+++ standalone/logger.js
@@ -4,7 +4,7 @@ var _ = require('lodash');
 
 
 // (set to `true` to display development-only log messages)
-FILE_PARSER_LOGGER_ENABLED = (process.DEBUG==='skipper');
+FILE_PARSER_LOGGER_ENABLED = (process.env.DEBUG==='skipper');
 
 var logger;
 if (global.FILE_PARSER_LOGGER_ENABLED) {
