7474803265196b7e0d296c62d416bdd40a5181eb
Mike McNeil <mike@balderdash.co>
2015-09-18 16:18:34 -0500

    Revert "cloneDeep breaks stuff. stop using it."
    This reverts commit 6388926e607decce9121947fd75c8176cc5faaf8.

--- standalone/Upstream/build-or-normalize-receiver.js
+++ standalone/Upstream/build-or-normalize-receiver.js
@@ -1,6 +1,13 @@
+/**
+ * Module dependencies
+ */
+
 var path = require('path');
 var _ = require('lodash');
 var DefaultFileAdapter = require('skipper-disk');
+
+
+
 
 /**
  * [buildOrNormalizeReceiver description]
@@ -8,14 +15,15 @@ var DefaultFileAdapter = require('skipper-disk');
  * @return {[type]}            [description]
  * @api private
  */
+
 module.exports = function buildOrNormalizeReceiver (opts) {
 
-  var receiver;
+  var receiver__;
 
   // If a receiver instance was passed in as the first argument, use it directly
   if (typeof opts === 'object' && opts.writable) {
-    receiver = opts;
-    return receiver;
+    receiver__ = opts;
+    return receiver__;
   }
 
   // If the first argument is undefined, treat it as an empty configuration object.
@@ -26,10 +34,12 @@ module.exports = function buildOrNormalizeReceiver (opts) {
 
   // Now we can normalize our receiver options
   //////////////////////////////////////////////////////////////////////////////////////////////////////
+  var receiverOpts = _.cloneDeep(opts);
+
 
   // At this point, we know we didn't receive a proper receiver instance, so we should have
   // ended up with a configuration object. Otherwise, this is an error.
-  if (typeof opts !== 'object') {
+  if (typeof receiverOpts !== 'object') {
     var err = new Error('No valid receiver specified!');
     e.code = 'E_USAGE';
     err.usage = USAGE;
@@ -39,9 +49,11 @@ module.exports = function buildOrNormalizeReceiver (opts) {
     throw err;
   }
 
+  // console.log('receiverOpts:',receiverOpts);
+
   // Determine the file adapter to use
   // (defaults to `DefaultFileAdapter`, defined above in the module dependencies at the top of this file)
-  var Adapter = opts.adapter || DefaultFileAdapter;
+  var Adapter = receiverOpts.adapter || DefaultFileAdapter;
 
   // Support Adapter as either a function
   // or a verbatim adapter object (pass it in directly)
@@ -50,8 +62,8 @@ module.exports = function buildOrNormalizeReceiver (opts) {
   }
 
   // Finally, build a default receiver stream with the specified options
-  receiver = Adapter.receive(opts);
+  receiver__ = Adapter.receive(receiverOpts);
 
   // and return it.
-  return receiver;
+  return receiver__;
 };
